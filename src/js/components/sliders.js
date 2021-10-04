ceee34b40adbfc5d030bb490cbf46e16b75cbf19
maxdenaro <maxarte99@gmail.com>
2021-10-04 15:24:48 +0300

    добавил секции услуг, портфолио, проектов и отзывов

---
+++ src/js/components/sliders.js
@@ -0,0 +1,64 @@
+const portSlider = document.querySelector('.portfolio-section__items');
+
+const portfolioSlider = new Swiper(portSlider, {
+  slidesPerView: 3,
+  spaceBetween: gap,
+  on: {
+    init: function () {
+      const activeSlide = portSlider.querySelector('.swiper-slide-active');
+
+      const nextActiveSlide = activeSlide.nextElementSibling;
+
+      const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+      activeSlide.classList.add('slider-visible');
+      nextActiveSlide.classList.add('slider-visible');
+      nextNextActiveSlide.classList.add('slider-visible');
+    },
+  },
+  navigation: {
+    nextEl: '.portfolio-section__next',
+    prevEl: '.portfolio-section__prev',
+  },
+});
+
+document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
+  const activeSlide = portSlider.querySelector('.swiper-slide-next');
+
+  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+    el.classList.remove('slider-visible');
+  });
+
+  if (activeSlide.previousElementSibling) {
+    const nextActiveSlide = activeSlide.previousElementSibling;
+    activeSlide.classList.add('slider-visible');
+    nextActiveSlide.classList.add('slider-visible');
+    activeSlide.nextElementSibling.classList.add('slider-visible');
+  }
+});
+
+document.querySelector('.portfolio-section__next').addEventListener('click', () => {
+  const activeSlide = portSlider.querySelector('.swiper-slide-active');
+
+  const nextActiveSlide = activeSlide.nextElementSibling;
+
+  const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+    el.classList.remove('slider-visible');
+  });
+
+  activeSlide.classList.add('slider-visible');
+  nextActiveSlide.classList.add('slider-visible');
+  nextNextActiveSlide.classList.add('slider-visible');
+});
+
+const testimonialsSlider = new Swiper('.testimonials__items', {
+  slidesPerView: 1,
+  spaceBetween: gap,
+  loop: true,
+  navigation: {
+    nextEl: '.testimonials__next',
+    prevEl: '.testimonials__prev',
+  },
+});
--- src/js/global.js
+++ src/js/global.js
@@ -1,1 +1,2 @@
-console.log('global');
+const bodyStyles = window.getComputedStyle(document.body);
+const gap = parseInt(bodyStyles.getPropertyValue('--grid-gap'));
