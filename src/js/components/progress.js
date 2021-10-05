4bb8f1ebd4e6a900d71cd6db20d91428986911ff
maxdenaro <maxarte99@gmail.com>
2021-10-05 17:07:32 +0300

    добавил секции фактов, статей и контакты

---
+++ src/js/components/progress.js
@@ -0,0 +1,44 @@
+// const circle = document.querySelector('.progress');
+
+// const progressAnimation = () => {
+//   let percentageProgress = Math.floor(70);
+
+//   let radius = circle.getAttribute('r');
+//   console.log(radius)
+//   let circleLength = 2 * Math.PI * radius;
+//   console.log(circleLength)
+//   circle.setAttribute('stroke-dasharray', circleLength);
+//   circle.setAttribute('stroke-dashoffset', circleLength - circleLength * percentageProgress / 100);
+// };
+
+// progressAnimation();
+
+const circles = document.querySelectorAll('.facts-element__circle');
+circles.forEach(el => {
+
+  if (el.dataset.percentage == 'true') {
+    let progress = el.querySelector('.progress');
+    let valueBlock = el.querySelector('.facts-element__value');
+    let radius = progress.getAttribute('r');
+    let circleLength = 2 * Math.PI * radius;
+    let full = el.dataset.full;
+    let value = el.dataset.value;
+    let percentageProgress = Math.floor(value / full * 100);
+    console.log(percentageProgress)
+    valueBlock.textContent = value;
+    progress.setAttribute('stroke-dasharray', circleLength);
+    progress.setAttribute('stroke-dashoffset', circleLength - circleLength * percentageProgress / 100);
+  } else {
+    let progress = el.querySelector('.progress');
+    let valueBlock = el.querySelector('.facts-element__value');
+    let radius = progress.getAttribute('r');
+    let circleLength = 2 * Math.PI * radius;
+    let percent = el.dataset.percent;
+    let percentageProgress = Math.floor(percent);
+    valueBlock.textContent = percent + '%';
+    progress.setAttribute('stroke-dasharray', circleLength);
+    progress.setAttribute('stroke-dashoffset', circleLength - circleLength * percentageProgress / 100);
+  }
+
+
+});
