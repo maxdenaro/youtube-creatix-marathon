34a33eb86c2d6617c2a2cff2d9b2cf72836506fb
maxdenaro <maxarte99@gmail.com>
2021-10-08 18:28:47 +0300

    добавлена первая часть внутренней страницы услуг

---
+++ src/js/components/accordions.js
@@ -0,0 +1,10 @@
+if (document.querySelector('.we-offer')) {
+  const accordion1 = new GraphAccordion('.accordion-1', {
+    speed: 300
+  });
+
+  const accordion2 = new GraphAccordion('.accordion-2', {
+    speed: 300
+  });
+}
+
