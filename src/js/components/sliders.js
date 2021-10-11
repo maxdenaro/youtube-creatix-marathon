e9807348e2e685245b69c8b555baa3da882a0b43
maxdenaro <maxarte99@gmail.com>
2021-10-11 15:11:25 +0300

    Добавил третью страницу полностью

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -1,4 +1,5 @@
 const portSlider = document.querySelector('.portfolio-section__items');
+const relatedSlider = document.querySelector('.related-projects__items');
 
 if (portSlider) {
   const portfolioSlider = new Swiper(portSlider, {
@@ -55,6 +56,60 @@ if (portSlider) {
   });
 }
 
+if (relatedSlider) {
+  const relatedProjSlider = new Swiper(relatedSlider, {
+    slidesPerView: 3,
+    spaceBetween: gap,
+    on: {
+      init: function () {
+        const activeSlide = relatedSlider.querySelector('.swiper-slide-active');
+
+        const nextActiveSlide = activeSlide.nextElementSibling;
+
+        const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        nextNextActiveSlide.classList.add('slider-visible');
+      },
+    },
+    navigation: {
+      nextEl: '.related-projects__next',
+      prevEl: '.related-projects__prev',
+    },
+  });
+
+  document.querySelector('.related-projects__prev').addEventListener('click', () => {
+    const activeSlide = relatedSlider.querySelector('.swiper-slide-next');
+
+    document.querySelectorAll('.related-projects__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
+    if (activeSlide.previousElementSibling) {
+      const nextActiveSlide = activeSlide.previousElementSibling;
+      activeSlide.classList.add('slider-visible');
+      nextActiveSlide.classList.add('slider-visible');
+      activeSlide.nextElementSibling.classList.add('slider-visible');
+    }
+  });
+
+  document.querySelector('.related-projects__next').addEventListener('click', () => {
+    const activeSlide = relatedSlider.querySelector('.swiper-slide-active');
+
+    const nextActiveSlide = activeSlide.nextElementSibling;
+
+    const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+    document.querySelectorAll('.related-projects__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
+    activeSlide.classList.add('slider-visible');
+    nextActiveSlide.classList.add('slider-visible');
+    nextNextActiveSlide.classList.add('slider-visible');
+  });
+}
 
 const testimonialsSlider = new Swiper('.testimonials__items', {
   slidesPerView: 1,
