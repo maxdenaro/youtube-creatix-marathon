53231cb7b41f3c066055163bd463a28c00a3345e
maxdenaro <maxarte99@gmail.com>
2021-10-18 15:11:53 +0300

    добавил страницы о нас, новостей, вакансии

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -142,3 +142,44 @@ if (workImages) {
     },
   });
 }
+
+const historySlider = document.querySelector('.history-slider');
+
+if (historySlider) {
+  const workSlider = new Swiper(historySlider, {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".history__next",
+      prevEl: ".history__prev",
+    },
+  });
+
+  workSlider.on('slideChange', function () {
+    console.log(workSlider.realIndex);
+
+    historyBtns.forEach(el => {
+      el.classList.remove('history-nav__btn--active');
+    });
+
+    document.querySelector(`.history-nav__btn[data-index="${workSlider.realIndex}"]`).classList.add('history-nav__btn--active');
+  });
+
+  const historyBtns = document.querySelectorAll('.history-nav__btn');
+
+  historyBtns.forEach((el, idx) => {
+    el.setAttribute('data-index', idx);
+
+    el.addEventListener('click', (e) => {
+      const index = e.currentTarget.dataset.index;
+
+      historyBtns.forEach(el => {
+        el.classList.remove('history-nav__btn--active');
+      });
+
+      e.currentTarget.classList.add('history-nav__btn--active');
+
+      workSlider.slideTo(index);
+    });
+  });
+}
